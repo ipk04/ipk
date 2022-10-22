fbff9e41d48d6129fa79a953a2bdb492cd927648
ipk04 <qjaahzzang85@naver.com>
2022-10-22 18:42:15 +0900

    commit test

--- 2022_10/15650.cpp
+++ 2022_10/15650.cpp
@@ -3,43 +3,39 @@
 #include<stdio.h>
 
 using namespace std;
-
-int arr[8];
-bool visit[8];
-vector<int> vec;
 int n,m;
-
-
+vector<int> arr;
+vector<bool> visit;
+vector<int> vec;
 void print(){
 	for(int i=0;i<vec.size();i++)
 		printf("%d ",vec[i]);
 	printf("\n");
 	return;
 }
-void dfs(int cnt){
-
+void dfs(int cnt,int start){
 	if(cnt==m){
 		print();
 		return;
 	}
 	
 	for(int i=0;i<n;i++){
-		if(visit[i]==true) continue;
-		visit[i]=true;
-		vec.emplace_back(i+1);
-		dfs(cnt+1);
+		if(visit[i]==true || arr[i]<start) continue;
+		visit[i] =true;
+		vec.push_back(arr[i]);
+		dfs(cnt+1,arr[i]);
 		vec.pop_back();
 		visit[i]=false;
-	}	
+	}
+	
 }
 
+
 int main(){
-	scanf("%d%d",&n,&m);
-	//vector<int> arr(n,0);
-	//vector<bool> visit(n,false);
+	scanf("%d %d",&n,&m);
 	for(int i=0;i<n;i++){
-		arr[i]=i+1;
-		visit[i]=false;
+		arr.push_back(i+1);
+		visit.push_back(false);
 	}
-	dfs(0);
+	dfs(0,0);
 }
